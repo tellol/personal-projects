1796c7c3ce62f596493f49ff402dfb42b5dd31f0
tellol <58122229+tellol@users.noreply.github.com>
2021-03-21 23:29:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const monthlyToggler = document.querySelector("btn-1");
+const annualyToggler = document.querySelector("btn-2");
+
+const basicPlan = document.getElementById("basic-price");
+const premiumPlan = document.getElementById("premium-price");
+const businessPlan = document.getElementById("business-price");
+
