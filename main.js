3fbfc92543c5e5d949eb1683b6cef4e7ff489b47
tellol <58122229+tellol@users.noreply.github.com>
2021-03-22 00:37:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-const monthlyToggler = document.querySelector("btn-1");
-const annualyToggler = document.querySelector("btn-2");
+var clicked = false;
+const anuallyBasic = "$439 <span> / anually </span>"
+const anuallyPremium = "$539 <span> / anually </span>"
+const anuallyBusiness = "$639 <span> / anually </span>"
 
-const basicPlan = document.getElementById("basic-price");
-const premiumPlan = document.getElementById("premium-price");
-const businessPlan = document.getElementById("business-price");
+function toggleMonthly(){
+	if(!clicked){
+		clicked = true;
+		document.getElementById("basic-price").innerHTML = "$39 <span> / monthly </span>"
+		document.getElementById("premium-price").innerHTML = "$49 <span> / monthly </span>"
+		document.getElementById("business-price").innerHTML = "$59 <span> / monthly </span>"
+	}
+	else{
+		clicked = false;
+	}
+}
 
+function toggleAnually(){
+	if(!clicked){
+		clicked = true;
+		document.getElementById("basic-price").innerHTML = anuallyBasic
+		document.getElementById("premium-price").innerHTML = anuallyPremium
+		document.getElementById("business-price").innerHTML = anuallyBusiness
+	}
+	else{
+		clicked = false;
+	}
+}
